e58245b0d7efffa5d6222a4dbfd83a23aa1bcdad
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2024-01-04 17:01:02 +0530

    value read from form

--- src/Components/AddNews.jsx
+++ src/Components/AddNews.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
-import Header from './Header'
+import React, { useState } from "react";
+import Header from "./Header";
 
 const AddNews = () => {
+  const [input, changeInput] = useState({
+    source_id: "", 
+    source_name: "",
+    author: "",
+    title:"",
+    description:"",
+    url: "",
+    urlToImage:"",
+    publishedAt: "",
+    content:""
+  });
+
+  const readValue=(e)=>{
+    changeInput({...input,[e.target.name]:e.target.value})
+  }
+
+  const submitValue=()=>{
+    console.log(input)
+  }
+
+
   return (
     <div>
       <Header />
@@ -13,52 +34,112 @@ const AddNews = () => {
                 <label htmlFor="" className="form-label">
                   Title
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="title"
+                  value={input.title}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <label htmlFor="" className="form-label">
                   Description
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="description"
+                  value={input.description}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <label htmlFor="" className="form-label">
                   Published At
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="publishedAt"
+                  value={input.publishedAt}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <label htmlFor="" className="form-label">
                   Author
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="author"
+                  value={input.author}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <label htmlFor="" className="form-label">
                   URL
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="url"
+                  value={input.url}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <label htmlFor="" className="form-label">
                   Content
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="content"
+                  value={input.content}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <label htmlFor="" className="form-label">
                   Source id
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="source_id"
+                  value={input.source_id}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
                 <label htmlFor="" className="form-label">
                   Source Name
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="source_name"
+                  value={input.source_name}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <label htmlFor="" className="form-label">
+                  Image
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="urlToImage"
+                  value={input.urlToImage}
+                  onChange={readValue}
+                />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-               <button className="btn btn-success">Add</button>
+                <button className="btn btn-success" onClick={submitValue}>Add</button>
               </div>
             </div>
           </div>
@@ -66,6 +147,6 @@ const AddNews = () => {
       </div>
     </div>
   );
-}
+};
 
-export default AddNews
+export default AddNews;
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Search = () => {
+
+  const [input,changeInput]=useState(
+    {
+      source_name:""
+    }
+  )
+
+  const readValue=(e)=>{
+    changeInput({...input,[e.target.name]:e.target.value})
+  }
+
+  const submitValue=()=>{
+    console.log(input)
+  }
   return (
     <div>
+      <Header />
 
-        <Header/>
-        
-        <div className="container">
-          <div className="row">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-              <div className="row g-3">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                  <label htmlFor="" className="form-label">Source Name</label>
-                  <input type="text" className="form-control" />
-
-                </div>
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                  <button className="btn btn-success">Search</button>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">
+                  Source Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="source_name"
+                  value={input.source_name}
+                  onChange={readValue}
+                />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success" onClick={submitValue}>
+                  Search
+                </button>
               </div>
             </div>
           </div>
         </div>
-        </div>
-  )
+      </div>
+    </div>
+  );
 }
 
 export default Search
